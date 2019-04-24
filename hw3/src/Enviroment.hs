7368e519fa97e349d0d8ff15f6ffe724ec914415
Lev Dovzhik <lev.dovzhik@corp.vk.com>
2019-04-24 16:18:41 +0300

    Add expansion for single command

--- hw3/src/Enviroment.hs
+++ hw3/src/Enviroment.hs
@@ -13,9 +13,9 @@ module Enviroment
 
 import Control.Monad.Cont (ContT)
 import Control.Monad.Trans (MonadTrans, lift)
-import Control.Monad.Writer (WriterT, tell)
+import Control.Monad.Writer (WriterT, writer, tell)
 import Data.Map.Strict (Map, findWithDefault, insert)
-import System.Exit (ExitCode)
+import System.Exit (ExitCode (..))
 
 newtype PosArgs =
   PosArgs (Map Int String)
@@ -34,15 +34,15 @@ getEnvVar key env = findWithDefault "" key $ envVars env
 class Monad m =>
       MonadConsole m
   where
-  readString :: m String
-  writeString :: String -> m ()
+  readString :: m (ExitCode, String)
+  writeString :: String -> m ExitCode
   callExternal :: FilePath -> FilePath -> [String] -> m ExitCode
   callExternalWithOutput ::
        FilePath -> FilePath -> [String] -> m (ExitCode, String)
 
 instance MonadConsole m => MonadConsole (WriterT String m) where
   readString = lift readString
-  writeString s = tell $ s ++ " "
+  writeString s = writer (ExitSuccess, s ++ " ")
   callExternal cwd exec args = do
     (code, output) <- lift $ callExternalWithOutput cwd exec args
     tell output
@@ -56,29 +56,3 @@ instance MonadConsole m => MonadConsole (ContT r m) where
   callExternal cwd exec args = lift $ callExternal cwd exec args
   callExternalWithOutput cwd exec args =
     lift $ callExternalWithOutput cwd exec args
--- class Monad m =>
---       MonadMockedOutput mm m
---   | m -> mm
---   where
---   getOutput :: m a -> mm String
---   sameReadString :: m String
---   mockedWriteString :: String -> m ()
---   mockedCallExternal :: FilePath -> FilePath -> [String] -> m ExitCode
---   sameCallExternalWithOutput ::
---        FilePath -> FilePath -> [String] -> m (ExitCode, String)
--- instance forall m. MonadConsole m =>
---          MonadMockedOutput m (WriterT String m) where
---   sameReadString = lift readString
---   mockedWriteString s = tell $ s ++ " "
---   getOutput = execWriterT
---   mockedCallExternal cwd exec args = do
---     (code, output) <- lift $ callExternalWithOutput cwd exec args
---     tell output
---     return code
---   sameCallExternalWithOutput cwd exec args =
---     lift $ callExternalWithOutput cwd exec args
--- instance (Monad m, MonadMockedOutput mm m) => MonadConsole m where
---   readString = sameReadString
---   writeString = mockedWriteString
---   callExternal = mockedCallExternal
---   callExternalWithOutput = sameCallExternalWithOutput
--- hw3/src/Interpreter.hs
+++ hw3/src/Interpreter.hs
@@ -12,14 +12,27 @@ import Control.Monad.Cont (MonadCont, callCC)
 import Control.Monad.Reader (MonadReader, ReaderT, ask, asks, local)
 import Control.Monad.State (MonadState, get, gets, put)
 import Control.Monad.Trans (lift)
+import Control.Monad.Catch (catch, SomeException (..))
 import Control.Monad.Writer (WriterT, execWriterT)
+import Data.Char (isSpace)
 import Data.IORef (IORef, readIORef, writeIORef)
+import Data.List.NonEmpty (NonEmpty (..))
+import Data.List.Split (wordsBy)
 import Enviroment (EnvState (..), MonadConsole (..), PosArgs, getEnvVar, getPosArg)
-import Structure (Arg, DollarExpr (..), Program)
-import System.Exit (ExitCode)
+import Structure (Arg, ArgFragment (..), DQFragment (..), DollarExpr (..), Program,
+                  SingleCommand (..))
+import System.Exit (ExitCode (..))
 import System.FilePath (FilePath, isAbsolute, (</>))
 import System.IO (hGetContents)
 import System.Process (StdStream (CreatePipe), createProcess, cwd, proc, std_out, waitForProcess)
+
+data MetaChar
+  = One Char
+  | Many String
+  deriving (Show)
+
+isMetaSpace (One c) = isSpace c
+isMetaSpace _       = False
 
 data MutableEnv = MutableEnv
   { mePosArgs  :: PosArgs
@@ -67,17 +80,25 @@ instance MonadState EnvState IOConsole where
       lift $ writeIORef stateRef newState
 
 instance MonadConsole IOConsole where
-  readString = IOConsole $ lift getLine
-  writeString s = IOConsole $ lift $ putStrLn s
+  readString = IOConsole $ doRead `catch` (\(SomeException _) -> return (ExitFailure 1, ""))
+    where
+      doRead = do
+        str <- lift getLine
+        return (ExitSuccess, str)
+  writeString s = IOConsole $ doWrite s `catch` (\(SomeException _) -> return $ ExitFailure 1) 
+    where
+      doWrite s = do
+        lift $ putStrLn s
+        return ExitSuccess 
   callExternal curDir exec args =
     IOConsole $ do
-      let processInfo =
-            (proc exec args) {cwd = Just curDir, std_out = CreatePipe}
+      let processInfo = (proc exec args) {cwd = Just curDir}
       (_, _, _, procHandle) <- lift $ createProcess processInfo
       lift $ waitForProcess procHandle
   callExternalWithOutput curDir exec args =
     IOConsole $ do
-      let processInfo = (proc exec args) {cwd = Just curDir}
+      let processInfo =
+            (proc exec args) {cwd = Just curDir, std_out = CreatePipe}
       (_, Just stdoutHandle, _, procHandle) <- lift $ createProcess processInfo
       code <- lift $ waitForProcess procHandle
       output <- lift $ hGetContents stdoutHandle
@@ -106,10 +127,58 @@ execMockedSubShell ::
   -> m String
 execMockedSubShell subProgram = execWriterT $ execSubShell subProgram
 
+trimLeft :: String -> String
+trimLeft = dropWhile isSpace
+
+trimRight :: String -> String
+trimRight s = reverse $ trimLeft $ reverse s
+
+trim :: String -> String
+trim = f . f
+  where
+    f = reverse . dropWhile isSpace
+
 calcDollarExpr ::
      (MonadReader PosArgs m, MonadState EnvState m, MonadConsole m, MonadCont m)
   => DollarExpr
   -> m String
 calcDollarExpr (PosArg num)            = asks $ getPosArg num
 calcDollarExpr (EnvVar key)            = gets $ getEnvVar key
-calcDollarExpr (InlineCall subProgram) = execMockedSubShell subProgram
+calcDollarExpr (InlineCall subProgram) = trim <$> execMockedSubShell subProgram
+
+calcDoubleQuotes ::
+     (MonadReader PosArgs m, MonadState EnvState m, MonadConsole m, MonadCont m)
+  => [DQFragment]
+  -> m String
+calcDoubleQuotes fragments = do
+  mappedFragments <- traverse mapper fragments
+  return $ concat mappedFragments
+  where
+    mapper (JustSymbol c)     = return [c]
+    mapper (Subst subProgram) = calcDollarExpr subProgram
+
+calcArg ::
+     (MonadReader PosArgs m, MonadState EnvState m, MonadConsole m, MonadCont m)
+  => Arg
+  -> m [String]
+calcArg arg = do
+  mappedFragments <- traverse mapToMetaChar arg
+  let metaString = concat mappedFragments
+  let splitedMetaString = wordsBy isMetaSpace metaString
+  return $ map (concatMap mapToString) splitedMetaString
+  where
+    mapToMetaChar (SingleQuotes sq) = return $ pure $ Many sq
+    mapToMetaChar (DoubleQuotes dq) = pure . Many <$> calcDoubleQuotes dq
+    mapToMetaChar (Symbol c)        = return $ pure $ One c
+    mapToMetaChar (Expr e)          = map One <$> calcDollarExpr e
+    mapToString (One c)  = [c]
+    mapToString (Many s) = s
+
+calcSingleCommand ::
+     (MonadReader PosArgs m, MonadState EnvState m, MonadConsole m, MonadCont m)
+  => SingleCommand
+  -> m [String]
+calcSingleCommand command = do
+  mappedName <- calcArg $ name command
+  mappedArgs <- traverse calcArg $ args command
+  return $ mappedName ++ mconcat mappedArgs
